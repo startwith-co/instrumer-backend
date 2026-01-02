a46beda6e2d70937971fe60aed55a25853db8f5b
euics <pjh575157@wjcompass.com>
2026-01-02 18:12:25 +0900

    feat: 회원정보 조회 API 통합

--- src/main/java/instrumers/backend/user/user/controller/UserController.java
+++ src/main/java/instrumers/backend/user/user/controller/UserController.java
@@ -1,99 +1,39 @@
 package instrumers.backend.user.user.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import instrumers.backend.base.BaseResponse;
-import instrumers.backend.common.service.CommonService;
-import instrumers.backend.exception.BadRequestException;
-import instrumers.backend.user.consumer.service.ConsumerService;
 import instrumers.backend.user.user.service.UserService;
-import instrumers.backend.user.vendor.service.VendorService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static instrumers.backend.user.user.controller.request.UserRequest.*;
-import static instrumers.backend.user.user.controller.response.UserResponse.*;
-import static instrumers.backend.user.consumer.controller.request.ConsumerRequest.*;
-import static instrumers.backend.user.vendor.controller.request.VendorRequest.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
-@Tag(name = "회원")
+@RequestMapping("/api/users")
+@Tag(name = "회원 권한 필요")
 public class UserController {
 	private final UserService userService;
-	private final CommonService commonService;
-	private final ConsumerService consumerService;
-	private final VendorService vendorService;
 
-	@PostMapping(value = "/public/login")
-	@Operation(summary = "회원 로그인")
-	public ResponseEntity<BaseResponse<LoginUserResponse>> register(@Valid @RequestBody LoginUserRequest request) {
-		LoginUserResponse response = userService.Login(request);
+	@GetMapping()
+	@Operation(summary = "사용자 조회")
+	public ResponseEntity<BaseResponse<Object>> get(HttpServletRequest httpServletRequest) {
+		Long userSeq = (Long)httpServletRequest.getAttribute("userSeq");
+		Object response = userService.get(userSeq);
 
 		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
 	}
 
-	@PostMapping(value = "/public/reissue-token")
-	@Operation(summary = "토큰 재발급")
-	public ResponseEntity<BaseResponse<ReIssueTokenResponse>> reissueToken(
-		@Valid @RequestBody ReissueTokenRequest request) {
-		ReIssueTokenResponse response = userService.reissueToken(request);
-
-		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
-	}
-
-	@PostMapping(value = "/public/send-email")
-	@Operation(summary = "이메일 전송 (유효시간 5분)")
-	public ResponseEntity<BaseResponse<Map<String, String>>> sendEmail(@Valid @RequestBody SendEmailRequest request) {
-		String authCode = commonService.sendAuthCode(request.email());
-		Map<String, String> response = new HashMap<>();
-		response.put("authCode", authCode);
-
-		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
-	}
-
-	@PostMapping(value = "/public/auth-code")
-	@Operation(summary = "이메일 인증 번호 확인")
-	public ResponseEntity<BaseResponse<String>> verifyAuthKey(@Valid @RequestBody VerifyEmailAuthKeyRequest request) {
-		if (!commonService.verifyAuthCode(request.email(), request.authCode())) {
-			throw new BadRequestException(
-				HttpStatus.BAD_REQUEST.value(),
-				"인증번호가 틀렸습니다."
-			);
-		}
-
-		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
-	}
-
-	@PostMapping(value = "/public/register/consumer")
-	@Operation(summary = "수요 고객 회원가입")
-	public ResponseEntity<BaseResponse<String>> registerConsumer(@Valid @RequestBody RegisterConsumerRequest request) {
-		consumerService.save(request);
-
-		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
-	}
-
-	@PostMapping(value = "/public/register/vendor")
-	@Operation(summary = "벤더 기업 회원가입")
-	public ResponseEntity<BaseResponse<String>> registerVendor(@Valid @RequestBody RegisterVendorRequest request) {
-		vendorService.save(request);
-
-		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
-	}
-
-	@DeleteMapping("/user")
+	@DeleteMapping()
 	@Operation(summary = "사용자 삭제")
-	public ResponseEntity<BaseResponse<String>> delete(HttpServletRequest request) {
-		Long userSeq = (Long)request.getAttribute("userSeq");
+	public ResponseEntity<BaseResponse<String>> delete(HttpServletRequest httpServletRequest) {
+		Long userSeq = (Long)httpServletRequest.getAttribute("userSeq");
 		userService.delete(userSeq);
 
 		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
---
+++ src/main/java/instrumers/backend/user/user/controller/UserPublicController.java
@@ -0,0 +1,92 @@
+package instrumers.backend.user.user.controller;
+
+import instrumers.backend.base.BaseResponse;
+import instrumers.backend.common.service.CommonService;
+import instrumers.backend.exception.BadRequestException;
+import instrumers.backend.user.consumer.service.ConsumerService;
+import instrumers.backend.user.user.service.UserService;
+import instrumers.backend.user.vendor.service.VendorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static instrumers.backend.user.user.controller.request.UserRequest.*;
+import static instrumers.backend.user.user.controller.response.UserResponse.*;
+import static instrumers.backend.user.consumer.controller.request.ConsumerRequest.*;
+import static instrumers.backend.user.vendor.controller.request.VendorRequest.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/public")
+@Tag(name = "회원 권한 불필요")
+public class UserPublicController {
+	private final UserService userService;
+	private final CommonService commonService;
+	private final ConsumerService consumerService;
+	private final VendorService vendorService;
+
+	@PostMapping(value = "/login")
+	@Operation(summary = "회원 로그인")
+	public ResponseEntity<BaseResponse<LoginUserResponse>> register(@Valid @RequestBody LoginUserRequest request) {
+		LoginUserResponse response = userService.Login(request);
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
+	}
+
+	@PostMapping(value = "/reissue-token")
+	@Operation(summary = "토큰 재발급")
+	public ResponseEntity<BaseResponse<ReIssueTokenResponse>> reissueToken(
+		@Valid @RequestBody ReissueTokenRequest request) {
+		ReIssueTokenResponse response = userService.reissueToken(request);
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
+	}
+
+	@PostMapping(value = "/send-email")
+	@Operation(summary = "이메일 전송 (유효시간 5분)")
+	public ResponseEntity<BaseResponse<Map<String, String>>> sendEmail(@Valid @RequestBody SendEmailRequest request) {
+		String authCode = commonService.sendAuthCode(request.email());
+		Map<String, String> response = new HashMap<>();
+		response.put("authCode", authCode);
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), response));
+	}
+
+	@PostMapping(value = "/auth-code")
+	@Operation(summary = "이메일 인증 번호 확인")
+	public ResponseEntity<BaseResponse<String>> verifyAuthKey(@Valid @RequestBody VerifyEmailAuthKeyRequest request) {
+		if (!commonService.verifyAuthCode(request.email(), request.authCode())) {
+			throw new BadRequestException(
+				HttpStatus.BAD_REQUEST.value(),
+				"인증번호가 틀렸습니다."
+			);
+		}
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
+	}
+
+	@PostMapping(value = "/register/consumer")
+	@Operation(summary = "수요 고객 회원가입")
+	public ResponseEntity<BaseResponse<String>> registerConsumer(@Valid @RequestBody RegisterConsumerRequest request) {
+		consumerService.save(request);
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
+	}
+
+	@PostMapping(value = "/register/vendor")
+	@Operation(summary = "벤더 기업 회원가입")
+	public ResponseEntity<BaseResponse<String>> registerVendor(@Valid @RequestBody RegisterVendorRequest request) {
+		vendorService.save(request);
+
+		return ResponseEntity.ok().body(BaseResponse.ofSuccess(HttpStatus.OK.value(), "SUCCESS"));
+	}
+}
